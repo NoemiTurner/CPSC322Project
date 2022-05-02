45be235dcbb24de99e9f6c2c952e11d4c03f9811
NoemiTurner <nturner3@zagmail.gonzaga.edu>
2022-05-02 05:05:45 +0000

    commit

---
+++ test_myclassifiers.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from mysklearn.myclassifiers import MyDecisionTreeClassifier
+
+# TODO: copy your test_myclassifiers.py solution from PA4-6 here
+
+def test_random_forest_classifier_fit():
+    assert False is True # TODO: fix this
+
+def test_random_forest_classifier_predict():
+    assert False is True # TODO: fix this
