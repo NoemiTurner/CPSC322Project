325221c2cdc80fdb8790dee02bca19781e7e1d84
NoemiTurner <nturner3@zagmail.gonzaga.edu>
2022-05-03 23:38:22 +0000

    Random forest pseudo code and mapping out functions and tests

--- mysklearn/myrandomforest.py
+++ mysklearn/myrandomforest.py
@@ -8,8 +8,9 @@ class MyRandomForestClassifier:
         y_train(list of obj): The target y values (parallel to X_train).
             The shape of y_train is n_samples
         tree(nested list): The extracted tree model.
+    
     Notes:
-        Loosely based on sklearn's DecisionTreeClassifier:
+        Loosely based on sklearn's RandomForestClassifier:
             https://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html
         Terminology: instance = sample = row and attribute = feature = column
     """
@@ -22,13 +23,74 @@ class MyRandomForestClassifier:
         self.M = M
         self.F = F
 
+    """
+    Build a forest of trees from the training set (X, y)
+    """
     def fit(self, X_train, y_train):
         
-        pass # TODO: fix this
-
+        return self
+    
+    """
+    Predict class for X
+    """
     def predict(self, X_test):
+        y = []
         
-        return [] # TODO: fix this
+        return y # returns the predicted classes 
 
+# 1. Generate a random stratified test set consisting of one third of the original data set, 
+# with the remaining two thirds of the instances forming the "remainder set".
+
+# 2. Generate N "random" decision trees using bootstrapping (giving a training and validation set)
+#  over the remainder set. At each node, build your decision trees by randomly selecting F of the 
+#  remaining attributes as candidates to partition on. This is the standard random forest approach 
+#  discussed in class. Note that to build your decision trees you should still use entropy; 
+#  however, you are selecting from only a (randomly chosen) subset of the available attributes.
+
+# 3. Select the M most accurate of the N decision trees using the corresponding validation sets.
+
+# 4. Use simple majority voting to predict classes using the M decision trees over the test set.
+# BONUS (2 pts): Modify your random forest algorithm to use the "track record" weighted voting scheme 
+# (instead of simple majority voting). See the weighted majority voting lab tasks on Github to help 
+# with this bonus task. Compare your results to those w/simple majority voting.
+
+# project notes
+# implement a random forest (all learners are trees)
+# with bagging and with random attribute subsets
+# will need to modify tree generation: for each node 
+# in our tree, we use random attribute subsets.
+# call compute_random_subset() right before a call to
+# select_attribute() in tdidt pass the return subset
+# (size F) into select_attribute()
+
+
+# Define a python function that selects F random attributes
+# from an attribute list
+# (test your function with att_indexes (or header))
+def compute_random_subset(values, num_values):
+    # there is a function np.random.choice()
+    values_copy = values[:] # shallow copy
+    np.random.shuffle(values_copy) # in place shuffle
+    return values_copy[:num_values]
+F = 2
+print(compute_random_subset(header, F))
+att_indexes = list(range(len(header)))
+print(compute_random_subset(att_indexes, F))
+
+
+# (done on PA5) Ensemble Lab Task 1: 
+# Write a bootstrap function to return a random sample of rows with replacement
+# (test your function with the interview dataset)
+def compute_bootstrapped_sample(table):
+    n = len(table)
+    sample = []
+    for _ in range(n):
+        rand_index = np.random.randint(0, n) # Return random integers from low (inclusive) to high (exclusive)
+        sample.append(table[rand_index])
+    return sample 
+
+sample = compute_bootstrapped_sample(table)
+for row in sample:
+    print(row)
 
  
--- test_myclassifiers.py
+++ test_myclassifiers.py
@@ -2,9 +2,32 @@ import numpy as np
 
 from mysklearn.myrandomforest import MyRandomForestClassifier
 
-# interview dataset
-HEADER_INTERVEIW = ["level", "lang", "tweets", "phd", "interviewed_well"]
-X_TRAIN_INTERVIEW = [
+# # interview dataset
+# HEADER_INTERVEIW = ["level", "lang", "tweets", "phd", "interviewed_well"]
+# X_TRAIN_INTERVIEW = [
+#     ["Senior", "Java", "no", "no"],
+#     ["Senior", "Java", "no", "yes"],
+#     ["Mid", "Python", "no", "no"],
+#     ["Junior", "Python", "no", "no"],
+#     ["Junior", "R", "yes", "no"],
+#     ["Junior", "R", "yes", "yes"],
+#     ["Mid", "R", "yes", "yes"],
+#     ["Senior", "Python", "no", "no"],
+#     ["Senior", "R", "yes", "no"],
+#     ["Junior", "Python", "yes", "no"],
+#     ["Senior", "Python", "yes", "yes"],
+#     ["Mid", "Python", "no", "yes"],
+#     ["Mid", "Java", "yes", "no"],
+#     ["Junior", "Python", "no", "yes"]
+# ]
+# Y_TRAIN_INTERVIEW = ["False", "False", "True", "True", "True", "False", "True", "False", "True", "True", "True", "True", "True", "False"]
+
+header = ["level", "lang", "tweets", "phd"]
+attribute_domains = {"level": ["Senior", "Mid", "Junior"], 
+    "lang": ["R", "Python", "Java"],
+    "tweets": ["yes", "no"], 
+    "phd": ["yes", "no"]}
+X = [
     ["Senior", "Java", "no", "no"],
     ["Senior", "Java", "no", "yes"],
     ["Mid", "Python", "no", "no"],
@@ -20,7 +43,20 @@ X_TRAIN_INTERVIEW = [
     ["Mid", "Java", "yes", "no"],
     ["Junior", "Python", "no", "yes"]
 ]
-Y_TRAIN_INTERVIEW = ["False", "False", "True", "True", "True", "False", "True", "False", "True", "True", "True", "True", "True", "False"]
+
+y = ["False", "False", "True", "True", "True", "False", "True", "False", "True", "True", "True", "True", "True", "False"]
+# stitch X and y together to make one table
+table = [X[i] + [y[i]] for i in range(len(X))]
+
+
+# For testing your MyRandomForestClassifier fit() I recommend using a small dataset from PA7's test 
+# cases that you can easily calculate entropy for. Then choose small values of N, M, and F. 
+# Then seed your random number generator and see what attributes will be selected in the F-sized subsets 
+# and what bootstrap samples will be generated for that seed. Then you can determine what the N trees 
+# will look like and what the M best ones are based on the validation sets.
+
+# predict() is much more straightforward, use majority voting amongst the M trees to make a prediction 
+# for an unseen instance, asserting it is the correct instance based on the trees.
 
 def test_random_forest_classifier_fit():
     rfc = MyRandomForestClassifier()
