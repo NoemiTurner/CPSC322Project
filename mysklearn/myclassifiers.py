1c9f45f30d4473b3b310d7792df1f7e14442f064
Adrian Abeyta <aabeyta@zagmail.gonzaga.edu>
2022-05-03 19:19:05 +0000

    decision tree done

--- mysklearn/myclassifiers.py
+++ mysklearn/myclassifiers.py
@@ -304,3 +304,127 @@ class MyNaiveBayesClassifier:
             indexes = myutils.find_max(posts)
             y_predicted.append(indexes)
         return y_predicted
+
+class MyDecisionTreeClassifier:
+    """Represents a decision tree classifier.
+ 
+    Attributes:
+        X_train(list of list of obj): The list of training instances (samples).
+                The shape of X_train is (n_train_samples, n_features)
+        y_train(list of obj): The target y values (parallel to X_train).
+            The shape of y_train is n_samples
+        tree(nested list): The extracted tree model.
+ 
+    Notes:
+        Loosely based on sklearn's DecisionTreeClassifier:
+            https://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html
+        Terminology: instance = sample = row and attribute = feature = column
+    """
+    def __init__(self):
+        """Initializer for MyDecisionTreeClassifier.
+        """
+        self.X_train = None
+        self.y_train = None
+        self.tree = None
+ 
+    def fit(self, X_train, y_train, F=None):
+        """Fits a decision tree classifier to X_train and y_train using the TDIDT
+        (top down induction of decision tree) algorithm.
+ 
+        Args:
+            X_train(list of list of obj): The list of training instances (samples).
+                The shape of X_train is (n_train_samples, n_features)
+            y_train(list of obj): The target y values (parallel to X_train)
+                The shape of y_train is n_train_samples
+            F (int): used to constrain attribute selection and force diversity
+ 
+        Notes:
+            Since TDIDT is an eager learning algorithm, this method builds a decision tree model
+                from the training data.
+            Build a decision tree using the nested list representation described in class.
+            On a majority vote tie, choose first attribute value based on attribute domain ordering.
+            Store the tree in the tree attribute.
+            Use attribute indexes to construct default attribute names (e.g. "att0", "att1", ...).
+        """
+        headert = []
+        domain = []
+        domain_dict = {}
+        for i in range(len(X_train[0])):
+            att_num = str(i)
+            headert.append("att" + att_num)
+        self.header = headert
+        for i in range(len(X_train[0])):
+            for row in X_train:
+                domain.append(row[i])
+            domain_dict[headert[i]]= list(np.unique(domain))
+            domain = []
+        train = [X_train[i] + [y_train[i]] for i in range(len(X_train))]
+        available_attributes = headert.copy()
+        self.tree = myutils.tdidt(train, available_attributes, domain_dict, headert, F)
+        # self.X_train = X_train
+        # self.y_train = y_train
+        # main_header = ["att" + str(i) for i in range(len(X_train[0]))]
+        # main_header.append("class") # Make the class column parallel with the y_train column
+        # main_table = [X_train[i] + [y_train[i]] for i in range(len(X_train))]
+        # self.tree = myutils.tdidt([main_table,main_table],main_header, F) # added F for attribute selection in randomforest
+ 
+    def predict(self, X_test):
+        """Makes predictions for test instances in X_test.
+ 
+        Args:
+            X_test(list of list of obj): The list of testing samples
+                The shape of X_test is (n_test_samples, n_features)
+ 
+        Returns:
+            y_predicted(list of obj): The predicted target y values (parallel to X_test)
+        """
+        # y_predicted = []
+        # for test_instance in X_test:
+        #     y_predicted.append(myutils.decision_traverse(self.tree,test_instance))
+        # return y_predicted
+        header = []
+        for i in range(len(X_test[0])):
+            att_num = str(i)
+            header.append("att" + att_num)
+        predictions = []
+        for item in X_test:
+            predictions.append(myutils.tdidt_predict(header, self.tree, item))
+        return predictions
+ 
+    def print_decision_rules(self, attribute_names=None, class_name="class"):
+        """Prints the decision rules from the tree in the format
+        "IF att == val AND ... THEN class = label", one rule on each line.
+ 
+        Args:
+            attribute_names(list of str or None): A list of attribute names to use in the decision rules
+                (None if a list is not provided and the default attribute names based on indexes
+                (e.g. "att0", "att1", ...) should be used).
+            class_name(str): A string to use for the class name in the decision rules
+                ("class" if a string is not provided and the default name "class" should be used).
+        """
+        # if attribute_names is None:
+        #     attribute_names = ["att" + str(i) for i in range(len(self.X_train[0]))]
+        # myutils.print_decision_rules_helper(self.tree)
+        myutils.print_tree_helper(self.tree, [], self.tree[0])
+ 
+    # BONUS method
+    def visualize_tree(self, dot_fname, pdf_fname, attribute_names=None):
+        """BONUS: Visualizes a tree via the open source Graphviz graph visualization package and
+        its DOT graph language (produces .dot and .pdf files).
+ 
+        Args:
+            dot_fname(str): The name of the .dot output file.
+            pdf_fname(str): The name of the .pdf output file generated from the .dot file.
+            attribute_names(list of str or None): A list of attribute names to use in the decision rules
+                (None if a list is not provided and the default attribute names based on indexes
+                (e.g. "att0", "att1", ...) should be used).
+ 
+        Notes:
+            Graphviz: https://graphviz.org/
+            DOT language: https://graphviz.org/doc/info/lang.html
+            You will need to install graphviz in the Docker container as shown in class to complete this method.
+        """
+        vis = gv.Graph(dot_fname,engine="dot",format="pdf")
+        tree = myutils.traverse_tree(self.tree,None,vis,num=0)
+        # output the vis as a pdf
+        vis.render(pdf_fname)
